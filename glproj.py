e5396d427f583d095341f0153287401461a6c9e0
Shaun Hills <shaun.hills@engineering.digital.dwp.gov.uk>
2025-05-13 14:57:37 +0100

    chore: add log level

--- glproj.py
+++ glproj.py
@@ -12,7 +12,7 @@ logging.basicConfig(level=logging.INFO, format='%(message)s')
 logger = logging.getLogger()
 file_handler = logging.FileHandler('script.log')
 file_handler.setLevel(logging.INFO)
-formatter = logging.Formatter('%(asctime)s - %(message)s')
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
 
